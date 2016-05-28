c537155431e34da2c8c08e84a9ff5d95e7802991
Phillip Bussart <phillip.bussart@gmail.com>
2016-05-28 13:39:03 -0600

    Added generic sprites and keyboard functionality

--- core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
+++ core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
@@ -1,52 +1,65 @@
 package com.packtpub.libgdx.canyonbunny;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.packtpub.libgdx.canyonbunny.game.WorldController;
 import com.packtpub.libgdx.canyonbunny.game.WorldRenderer;
 
-
 public class CanyonBunnyMain implements ApplicationListener {
 	private static final String TAG = CanyonBunnyMain.class.getName();
-	
+
 	private WorldController worldController;
-	private WorldRenderer worlRenderer;
-	
-	
+	private WorldRenderer worldRenderer;
+	private boolean paused;
+
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-		
+		// Set Libgdx log level to DEBUG
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		// Initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
+		// Game world is active on start
+		paused = false;
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
+		worldRenderer.resize(width, height);
 	}
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
-		
+		// do not update game world when paused
+		if (!paused) {
+			// Update game world by the time that has passed since last rendered
+			// frame
+			worldController.update(Gdx.graphics.getDeltaTime());
+		}
+		// Sets the clear screen color to: CornFlower Blue
+		Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f, 0xff / 255.0f);
+		// Clears the screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// Render game world to screen
+		worldRenderer.render();
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
+		paused = true;
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
+		paused = false;
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		worldRenderer.dispose();
 	}
 
-	
 }
