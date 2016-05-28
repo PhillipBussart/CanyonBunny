42860b0d54dea7471cb3a220dbde8b0e838052ca
Phillip Bussart <phillip.bussart@gmail.com>
2016-05-28 11:16:46 -0600

    fixed last commit

--- desktop/src/com/packtpub/libgdx/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/com/packtpub/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -8,5 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new CanyonBunnyMain(), config);
+		config.title = "CanyonBunny";
+		config.width = 480;
+		config.height = 320;
 	}
 }
